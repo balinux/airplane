8fc7be70c8010637e4db1b962e1c6dd97020966e
balinux <ryojuniyantara@gmail.com>
2023-02-18 14:44:19 +0800

    Slicing get started

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:airplane/UI/pages/get_started_page.dart';
 import 'package:airplane/UI/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const SplashPage(),
+      home: const GetStartedPage(),
     );
   }
 }
