75c1cf3ffa5e5761fa5d866a61257545c9919b03
balinux <ryojuniyantara@gmail.com>
2023-02-22 11:13:44 +0800

    Slicing Home - Bagian Popular Destinations

--- lib/UI/pages/home-page.dart
+++ lib/UI/pages/home-page.dart
@@ -1,3 +1,4 @@
+import 'package:airplane/UI/widgets/destination_card.dart';
 import 'package:airplane/shared/theme.dart';
 import 'package:flutter/material.dart';
 
@@ -6,6 +7,7 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // header
     Widget header() {
       return Container(
         margin:
@@ -44,8 +46,51 @@ class HomePage extends StatelessWidget {
       );
     }
 
+    Widget popularDestination() {
+      return Container(
+        margin: const EdgeInsets.only(top: 30),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: const [
+              DestinationCard(
+                name: 'Lake Ciliwung',
+                city: 'Tangerang',
+                imageUrl: 'assets/image_destination1.png',
+                rating: 4.5,
+              ),
+              DestinationCard(
+                name: 'White Houses',
+                city: 'Spain',
+                imageUrl: 'assets/image_destination2.png',
+                rating: 4.7,
+              ),
+              DestinationCard(
+                name: 'Hill Heyo',
+                city: 'Monaco',
+                imageUrl: 'assets/image_destination3.png',
+                rating: 4.8,
+              ),
+              DestinationCard(
+                name: 'Menarra',
+                city: 'Japan',
+                imageUrl: 'assets/image_destination4.png',
+                rating: 4.5,
+              ),
+              DestinationCard(
+                name: 'Payung Teduh',
+                city: 'Singapore',
+                imageUrl: 'assets/image_destination5.png',
+                rating: 4.8,
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
     return ListView(
-      children: [header()],
+      children: [header(), popularDestination()],
     );
   }
 }
