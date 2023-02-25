cd827db011567fdb701669530eb8a0f79e16a7d0
balinux <ryojuniyantara@gmail.com>
2023-02-26 00:52:41 +0800

    Membuat Page Cubit

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'package:airplane/UI/pages/get_started_page.dart';
 import 'package:airplane/UI/pages/main_page.dart';
 import 'package:airplane/UI/pages/sign_up_page.dart';
 import 'package:airplane/UI/pages/splash_page.dart';
+import 'package:airplane/cubit/page_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,20 +17,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => PageCubit(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        // home: const SplashPage(),
+        routes: {
+          '/': (context) => const SplashPage(),
+          '/get-started': (context) => const GetStartedPage(),
+          '/sign-up': (context) => const SignUpPage(),
+          '/bonus': (context) => const BonusPage(),
+          '/main': (context) => const MainPage(),
+          '/home': (context) => const MainPage()
+        },
       ),
-      // home: const SplashPage(),
-      routes: {
-        '/': (context) => const SplashPage(),
-        '/get-started': (context) => const GetStartedPage(),
-        '/sign-up': (context) => const SignUpPage(),
-        '/bonus': (context) => const BonusPage(),
-        '/main': (context) => const MainPage(),
-        '/home': (context) => const MainPage()
-      },
     );
   }
 }
