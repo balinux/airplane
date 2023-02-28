b08d9cf19c070283ceec3743988ce950a766152d
balinux <ryojuniyantara@gmail.com>
2023-03-01 07:13:47 +0800

    Menambahkan Sign Up Auth Service & User Service

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,33 @@
+import 'package:airplane/models/user_model.dart';
+import 'package:airplane/services/user_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<UserModel> signUp(
+      {required String email,
+      required String password,
+      required String name,
+      String hobby = ''}) async {
+    try {
+      UserCredential userCredential = await _auth
+          .createUserWithEmailAndPassword(email: email, password: password);
+
+      // menyimpan data ke firestore
+      UserModel user = UserModel(
+          id: userCredential.user!.uid,
+          email: email,
+          name: name,
+          hobby: hobby,
+          balance: 280000000);
+
+      // register ke firestore
+      await UserService().setuser(user);
+
+      return user;
+    } catch (e) {
+      throw e;
+    }
+  }
+}
