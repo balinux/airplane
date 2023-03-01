2188ecd93c7509e1b85f92745caddb5a0ceec368
balinux <ryojuniyantara@gmail.com>
2023-03-01 22:35:42 +0800

    Membuat Fitur Sign Out

--- lib/UI/pages/settings_page.dart
+++ lib/UI/pages/settings_page.dart
@@ -1,12 +1,41 @@
+import 'package:airplane/UI/widgets/custom_button.dart';
+import 'package:airplane/cubit/auth_cubit.dart';
+import 'package:airplane/shared/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Setting Page'),
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        // TODO: implement listener
+        if (state is AuthFailed) {
+          ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(backgroundColor: kRedColor, content: Text(state.error)));
+        } else if (state is AuthInitial) {
+          Navigator.pushNamedAndRemoveUntil(
+              context, '/sign-up', (route) => false);
+        }
+      },
+      builder: (context, state) {
+        if (state is AuthLoading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        return Center(
+          child: CustomButton(
+            title: 'Sign Out',
+            onPressed: () {
+              context.read<AuthCubit>().signOut();
+            },
+            width: 220,
+          ),
+        );
+      },
     );
   }
 }
