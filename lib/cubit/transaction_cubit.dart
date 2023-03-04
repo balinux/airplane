19c5909325c73b6e94ba9bed9782eeaf4fe3f746
balinux <ryojuniyantara@gmail.com>
2023-03-04 09:42:50 +0800

    Menambahkan Method Fetch Transactions

--- lib/cubit/transaction_cubit.dart
+++ lib/cubit/transaction_cubit.dart
@@ -12,9 +12,20 @@ class TransactionCubit extends Cubit<TransactionState> {
     try {
       emit(TransactionLoading());
       await TransactionService().createTransaction(transaction);
-      emit(TransactionSuccess());
+      emit(TransactionSuccess([]));
     } catch (e) {
       emit(TransactionFailed(e.toString()));
+    }
+  }
+
+  void fetchtransaction() async {
+    try {
+      emit(TransactionLoading());
+      List<TransactionModel> transactions =
+          await TransactionService().fetchTransactions();
+      emit(TransactionSuccess(transactions));
+    } catch (e) {
+      TransactionFailed(e.toString());
     }
   }
 }
--- lib/models/transaction_model.dart
+++ lib/models/transaction_model.dart
@@ -2,6 +2,7 @@ import 'package:airplane/models/destination_model.dart';
 import 'package:equatable/equatable.dart';
 
 class TransactionModel extends Equatable {
+  final String id;
   final DestinationModel destination;
   final int amountOfTraveler;
   final String selectedSeats;
@@ -12,7 +13,8 @@ class TransactionModel extends Equatable {
   final int grandTotal;
 
   TransactionModel(
-      {required this.destination,
+      {this.id = '',
+      required this.destination,
       this.amountOfTraveler = 0,
       this.selectedSeats = '',
       this.insurance = false,
@@ -20,6 +22,19 @@ class TransactionModel extends Equatable {
       this.vat = 0,
       this.price = 0,
       this.grandTotal = 0});
+
+  factory TransactionModel.fromJson(String id, Map<String, dynamic> json) =>
+      TransactionModel(
+          id: id,
+          destination: DestinationModel.fromJson(
+              json['destination']['id'], json['destination']),
+          amountOfTraveler: json['amountOfTraveler'],
+          selectedSeats: json['selectedSeats'],
+          insurance: json['insurance'],
+          refunable: json['refunable'],
+          vat: json['vat'],
+          price: json['price'],
+          grandTotal: json['grandTotal']);
 
   @override
   // TODO: implement props
