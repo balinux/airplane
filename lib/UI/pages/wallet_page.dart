9c98ddbf3ecb4162e00247b5056930452408084d
balinux <ryojuniyantara@gmail.com>
2023-03-04 10:26:39 +0800

    Mengubah Halaman Wallet

--- lib/UI/pages/wallet_page.dart
+++ lib/UI/pages/wallet_page.dart
@@ -1,12 +1,91 @@
+import 'package:airplane/cubit/auth_cubit.dart';
+import 'package:airplane/shared/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class WalletPage extends StatelessWidget {
   const WalletPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Wallet Page'),
+    return BlocBuilder<AuthCubit, AuthState>(
+      builder: (context, state) {
+        if (state is AuthSuccess) {
+          return Center(
+            child: Container(
+              padding: EdgeInsets.all(defaultMargin),
+              width: 300,
+              height: 211,
+              decoration: BoxDecoration(
+                  image: const DecorationImage(
+                      image: AssetImage('assets/image_card.png')),
+                  boxShadow: [
+                    BoxShadow(
+                        color: kPrimaryColor.withOpacity(.5),
+                        blurRadius: 50,
+                        offset: const Offset(0, 10))
+                  ]),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Name",
+                              style: whiteTextStyle.copyWith(fontWeight: light),
+                            ),
+                            Text(
+                              state.user.name,
+                              style: whiteTextStyle.copyWith(
+                                  fontWeight: medium, fontSize: 20),
+                              overflow: TextOverflow.ellipsis,
+                            )
+                          ],
+                        ),
+                      ),
+                      Container(
+                        width: 24,
+                        height: 24,
+                        margin: const EdgeInsets.only(right: 6),
+                        decoration: const BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage('assets/icon_plane.png'))),
+                      ),
+                      Text(
+                        'Pay',
+                        style: whiteTextStyle.copyWith(
+                            fontSize: 16, fontWeight: medium),
+                      )
+                    ],
+                  ),
+                  // balance
+                  const SizedBox(
+                    height: 41,
+                  ),
+                  Text(
+                    'Balance',
+                    style: whiteTextStyle.copyWith(fontWeight: light),
+                  ),
+                  // const SizedBox(
+                  //   height: 41,
+                  // ),
+                  Text(
+                    'IDR 280.000.000',
+                    style: whiteTextStyle.copyWith(
+                        fontWeight: medium, fontSize: 26),
+                  ),
+                ],
+              ),
+            ),
+          );
+        } else {
+          return const SizedBox();
+        }
+      },
     );
   }
 }
